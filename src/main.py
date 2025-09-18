ad31c28267daaefbc2a69cfcf26fe209d60321a7
Abner Freitas <defreitasabner@gmail.com>
2025-09-18 20:03:58 -0300

    Desenvolvida a nova pipeline de vacinação completa. Removida pipeline antiga.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,5 @@
 import logging
 
-import settings
-import pipelines
-import utils
-from gerenciador_google_drive import GerenciadorGoogleDrive
 
 logging.basicConfig(
     level = logging.INFO,
@@ -15,30 +11,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 def main():
-    # Preparação do ambiente local e remoto
-    settings.criar_estrutura_basica_de_diretorios()
-
-    google_drive = None
-    # try:
-    #     google_drive = GerenciadorGoogleDrive(
-    #         diretorio_dados = settings.GOOGLE_DRIVE_ROOT_DIR, 
-    #         diretorio_autenticacao = settings.AUTH_DIR
-    #     )
-    # except Exception as e:
-    #     logger.error(f'Não foi possível inicializar o gerenciador do Google Drive: {e}')
-    #     raise e
-
-    # Execução da pipeline de dados
-    pipelines.processar_dados_vacinacao_por_ano(
-        ano = 2021,
-        remover_arquivo_zip = False
-    )
-
-    # Upload dos dados processados para o Google Drive
-    if google_drive:
-        caminho_dados_processados_compactados = utils.compactar_arquivos(settings.VACINACAO_PROCESSED_DATA_DIR, 'dados_vacinacao.zip')
-        google_drive.upload_arquivo(caminho_dados_processados_compactados)
-        utils.remover_arquivo(caminho_dados_processados_compactados)
+    # TODO: implementar CLI para acesso às funcionalidades das pipelines
+    pass
 
 
 if __name__ == '__main__':
